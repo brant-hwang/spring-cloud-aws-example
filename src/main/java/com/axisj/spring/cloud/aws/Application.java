8c21240e049cef93aa80fb05df8dec2290ea06b1
Brant Hwang <dlstj3039@gmail.com>
2015-07-27 23:06:54 +0900

    import Optimize

--- src/main/java/com/axisj/spring/cloud/aws/Application.java
+++ src/main/java/com/axisj/spring/cloud/aws/Application.java
@@ -2,7 +2,6 @@ package com.axisj.spring.cloud.aws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class Application {
